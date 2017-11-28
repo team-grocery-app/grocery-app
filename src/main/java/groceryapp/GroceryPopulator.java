3012153c40f3c604ed7f9bcec021ac170ddf4c7a
jobidike <jobidike@gmail.com>
2017-11-28 14:21:34 -0500

    changes

--- src/main/java/groceryapp/GroceryPopulator.java
+++ src/main/java/groceryapp/GroceryPopulator.java
@@ -3,12 +3,16 @@ package groceryapp;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class GroceryPopulator implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		
+		Recipe recipe1 = new Recipe
+				
+		
 		
 	}
 
