fe8bb9a55a4dd54d85221ecf566130a09ec8e224
GeneCPChung <genecpc@gmail.com>
2017-11-28 12:48:43 -0500

    Add SelectedItemsRepo

---
+++ src/main/java/groceryapp/GroceryAppController.java
@@ -0,0 +1,52 @@
+package groceryapp;
+
+import javax.annotation.Resource;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class GroceryAppController {
+
+	@Resource
+	RecipeRepository recipeRepo;
+
+	@Resource
+	IngredientRepository ingredientRepo;
+
+	@Resource
+	StoreItemRepository storeItemRepo;
+
+	@Resource
+	LineItemRepository lineItemRepo;
+
+	@Resource
+	ShoppingListRepository shoppingListRepo;
+
+	@Resource
+	SelectedIngredientsRepository selectedIngredientsRepo;
+
+	@RequestMapping("/recipes")
+	public String getAllRecipes(Model model) {
+		model.addAttribute("recipes", recipeRepo.findAll());
+		return "recipes";
+	}
+
+	@RequestMapping("/recipe")
+	public String getOneRecipe(@RequestParam Long id, Model model) {
+		model.addAttribute("recipe", recipeRepo.findOne(id));
+		return "recipe";
+	}
+
+	@RequestMapping("/ingredients")
+	public String getAllIngredients(Model model) {
+		model.addAttribute("ingredients", ingredientRepo.findAll());
+		return "ingredients";
+	}
+
+	@RequestMapping("/selectedIngredients")
+	public String getAllSelectedIngredients(Model model) {
+		model.addAttribute("selectedIngredients", selectedIngredientsRepo.findAll());
+		return "selectedIngredients";
+	}
+}
