01915c87b09f62d5a83686b2136e0fbc77dad047
Tiffany Chuang <tiffany.chuang2@gmail.com>
2017-12-09 22:37:39 -0500

    start on detailing css, begin js

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,31 @@
+//copyright hover
+let copyright = document.querySelectorAll('.copyright');
+ for(let i=0; i<copyright.length; i++) {
+ 	copyright[i].addEventListener('mouseover', function() {
+ 		this.innerText = 'Created By Aaron Barclay, Tiffany Chuang, Gene Chung, Leslie Geissler, Jesse Snyder, and Mary Ziegler';
+ 	});
+ 	copyright[i].addEventListener('mouseout', function() {
+ 		this.innerText = '© 2017';
+ 	});
+ }
+
+//font increase on hover
+  let links = document.querySelectorAll('a');
+ for(let i=0; i<links.length; i++) {
+ 	links[i].addEventListener('mouseover', function() {
+ 		this.style.fontSize = '120%';
+ 	});
+ 	links[i].addEventListener('mouseout', function() {
+ 		this.style.fontSize = '22px';
+ 	});
+ }
+
+//delete selected ingredient
+ let deleteItem = document.querySelectorAll('#delete-button');
+for (let i = 0; i < deleteItem.length; i++) {
+	deleteItem[i].addEventListener('click', function(event) {
+		if (!confirm('Are you sure you would like to remove this item?  Once you remove this item, you will not be able to undo the change.')) {
+			event.preventDefault();
+		}
+	});
+}
