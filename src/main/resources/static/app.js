258cea85951b649c16ecc0877927d4e75f713248
Tiffany Chuang <tiffany.chuang2@gmail.com>
2017-12-12 18:07:27 -0500

    stopping point before work

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,7 +2,7 @@
 let copyright = document.querySelectorAll('.copyright');
  for(let i=0; i<copyright.length; i++) {
  	copyright[i].addEventListener('mouseover', function() {
- 		this.innerText = 'Created By Aaron Barclay, Tiffany Chuang, Gene Chung, Leslie Geissler, Jesse Snyder, and Mary Ziegler';
+ 		this.innerText = 'Created By Aaron Barclay, Tiffany Chuang, Gene Chung, Leslie Geissler, Jaimie Obidike, Jesse Snyder, and Mary Ziegler';
  	});
  	copyright[i].addEventListener('mouseout', function() {
  		this.innerText = 'Copyright © 2017';
